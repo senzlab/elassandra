3156da8a1a509fd4b1ba4cd061fe1968f0339573
eranga bandara <eranga.herath@pagero.com>
2018-04-29 22:12:21 +0530

    tweak start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "start cassandra..."
 
+# create dirs
 mkdir -p /var/log/cassandra
 chown -R cassandra:cassandra /var/log/cassandra
 chmod -R 775 /var/log/cassandra
 chown -R cassandra:cassandra /opt/elassandra
 
+# start to fire etcd alarms
 /usr/local/bin/etcd-watch.sh > /dev/null 2>&1 &
 
+# start cassandra
+# -e for start elassandra
+# -f for start foreground
 exec sudo -u cassandra /opt/elassandra/bin/cassandra -e -f
